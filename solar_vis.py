3c0c03ee15e47034b1fd23424dbcf76562ce5502
mskondratenko <kondratenko.ms@phystech.edu>
2020-11-13 16:33:24 +0300

    fixed drawableobjects class

--- solar_vis.py
+++ solar_vis.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 # license: GPLv3
 
+import pygame as pg
+
 """Модуль визуализации.
 Нигде, кроме этого модуля, не используются экранные координаты объектов.
 Функции, создающие гaрафические объекты и перемещающие их на экране, принимают физические координаты
@@ -15,7 +17,7 @@ window_width = 800
 window_height = 800
 """Высота окна"""
 
-scale_factor = None
+scale_factor = 1
 """Масштабирование экранных координат по отношению к физическим.
 Тип: float
 Мера: количество пикселей на один метр."""
@@ -24,7 +26,7 @@ scale_factor = None
 def calculate_scale_factor(max_distance):
     """Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине"""
     global scale_factor
-    scale_factor = 0.4*min(window_height, window_width)/max_distance
+    scale_factor = 0.5 * min(window_height, window_width) / max_distance
     print('Scale factor:', scale_factor)
 
 
@@ -33,13 +35,11 @@ def scale_x(x):
     Принимает вещественное число, возвращает целое число.
     В случае выхода **x** координаты за пределы экрана возвращает
     координату, лежащую за пределами холста.
-
     Параметры:
-
     **x** — x-координата модели.
     """
 
-    return int(x*scale_factor) + window_width//2
+    return int(x * scale_factor) + window_width // 2
 
 
 def scale_y(y):
@@ -48,69 +48,34 @@ def scale_y(y):
     В случае выхода **y** координаты за пределы экрана возвращает
     координату, лежащую за пределами холста.
     Направление оси развёрнуто, чтобы у модели ось **y** смотрела вверх.
-
     Параметры:
-
     **y** — y-координата модели.
     """
-
-    return int(x*scale_factor) + window_width//2
-
-
-def create_star_image(space, star):
-    """Создаёт отображаемый объект звезды.
-
-    Параметры:
-
-    **space** — холст для рисования.
-    **star** — объект звезды.
-    """
-
-    x = scale_x(star.x)
-    y = scale_y(star.y)
-    r = star.R
-    star.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=star.color)
-
-
-def create_planet_image(space, planet):
-    """Создаёт отображаемый объект планеты.
-
-    Параметры:
-
-    **space** — холст для рисования.
-    **planet** — объект планеты.
-    """
-    pass  # FIXME: сделать как у звезды
-
-
-def update_system_name(space, system_name):
-    """Создаёт на холсте текст с названием системы небесных тел.
-    Если текст уже был, обновляет его содержание.
-
-    Параметры:
-
-    **space** — холст для рисования.
-    **system_name** — название системы тел.
-    """
-    space.create_text(30, 80, tag="header", text=system_name, font=header_font)
-
-
-def update_object_position(space, body):
-    """Перемещает отображаемый объект на холсте.
-
-    Параметры:
-
-    **space** — холст для рисования.
-    **body** — тело, которое нужно переместить.
-    """
-    x = scale_x(body.x)
-    y = scale_y(body.y)
-    r = body.R
-    if x + r < 0 or x - r > window_width or y + r < 0 or y - r > window_height:
-        space.coords(body.image, window_width + r, window_height + r,
-                     window_width + 2*r, window_height + 2*r)  # положить за пределы окна
-    space.coords(body.image, x - r, y - r, x + r, y + r)
+    return int(y * scale_factor) + window_height // 2
 
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
+
+
+class Drawer:
+    def __init__(self, screen):
+        self.screen = screen
+
+    def update(self, figures, ui):
+        self.screen.fill((0, 0, 0))
+        for figure in figures:
+            figure.draw(self.screen)
+
+        ui.blit()
+        ui.update()
+        pg.display.update()
+
+
+class DrawableObject:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def draw(self, surface):
+        pg.draw.circle(surface, self.obj.color, (self.obj.x, self.obj.y),
+                       self.obj.R)
